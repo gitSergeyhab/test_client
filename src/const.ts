b53c27eaade1cd6d86f82d019a95d51694a35b26
Sergey <blackmoloch.cc@gmail.com>
2023-07-17 13:47:41 +0300

    change serverurl - returned

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
 export const {isDev} = process.env;
 
-// export const SERVER_URL = isDev ? 'http://localhost:5000/' : 'http://62.217.182.231:5000/';
-export const SERVER_URL =  'http://localhost:5000/';
+export const SERVER_URL = isDev ? 'http://localhost:5000/' : 'http://62.217.182.231:5000/';
+// export const SERVER_URL =  'http://localhost:5000/';
 
